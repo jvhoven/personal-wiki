d9a4239d94c3c6882af2870578f19e8ebfdb593c
jvhoven <jeffreyvanhoven@gmail.com>
2017-12-02 16:38:07 +0100

    Basic sqlite setup

---
+++ include/util.hpp
@@ -0,0 +1,94 @@
+//
+// Created by Jeffrey on 01/12/2017.
+//
+#ifndef PERSONALWIKI_UTIL_H
+#define PERSONALWIKI_UTIL_H
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include <zlib.h>
+
+class Util {
+public:
+    static std::string compress_string(const std::string& str, int compressionlevel = Z_BEST_COMPRESSION) {
+        z_stream zs;
+        memset(&zs, 0, sizeof(zs));
+
+        if (deflateInit(&zs, compressionlevel) != Z_OK)
+            throw(std::runtime_error("deflateInit failed while compressing."));
+
+        zs.next_in = (Bytef*)str.data();
+        zs.avail_in = str.size();
+
+        int ret;
+        char outbuffer[32768];
+        std::string outstring;
+
+        do {
+            zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+            zs.avail_out = sizeof(outbuffer);
+
+            ret = deflate(&zs, Z_FINISH);
+
+            if (outstring.size() < zs.total_out) {
+                outstring.append(outbuffer,
+                                 zs.total_out - outstring.size());
+            }
+        } while (ret == Z_OK);
+
+        deflateEnd(&zs);
+
+        if (ret != Z_STREAM_END) {
+            std::ostringstream oss;
+            oss << "Exception during zlib compression: (" << ret << ") " << zs.msg;
+            throw(std::runtime_error(oss.str()));
+        }
+
+        return outstring;
+    }
+
+    static std::string decompress_string(const std::string& str) {
+        z_stream zs;
+        memset(&zs, 0, sizeof(zs));
+
+        if (inflateInit(&zs) != Z_OK)
+            throw(std::runtime_error("inflateInit failed while decompressing."));
+
+        zs.next_in = (Bytef*)str.data();
+        zs.avail_in = str.size();
+
+        int ret;
+        char outbuffer[32768];
+        std::string outstring;
+
+        do {
+            zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+            zs.avail_out = sizeof(outbuffer);
+
+            ret = inflate(&zs, 0);
+
+            if (outstring.size() < zs.total_out) {
+                outstring.append(outbuffer,
+                                 zs.total_out - outstring.size());
+            }
+
+        } while (ret == Z_OK);
+
+        inflateEnd(&zs);
+
+        if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
+            std::ostringstream oss;
+            oss << "Exception during zlib decompression: (" << ret << ") "
+                << zs.msg;
+            throw(std::runtime_error(oss.str()));
+        }
+
+        return outstring;
+    }
+};
+
+#endif
